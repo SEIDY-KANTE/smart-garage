61dd32f838da5b8401422cb473a6ceb27727d3df
Bedru umer <bedru2019@gmail.com>
2022-12-24 21:07:30 +0300

    configured for deployment

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
-export const API_KEY = '5VJNHKSK83GNMH3O';
+export const API_KEY = '0XSQR8YK5AOV23BO';
 export const GARAGE_DOOR_WRITE_API_URL = `https://api.thingspeak.com/update?api_key=${API_KEY}&field1=`;
 export const DELIVERY_BOX_WRITE_API_URL = `https://api.thingspeak.com/update?api_key=${API_KEY}&field2=`;
